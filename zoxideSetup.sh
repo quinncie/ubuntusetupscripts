8052d8d23e2ab8d78a6eda63960119ef08c2d2b5
Quinn C <160205419+quinncie@users.noreply.github.com>
2024-02-20 10:18:13 -0700

    Update zoxideSetup.sh

--- zoxideSetup.sh
+++ zoxideSetup.sh
@@ -2,5 +2,5 @@ curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | 
 sudo apt-get install -y fzf
 echo "PATH=/home/$USER/.local/bin:$PATH" >> .bashrc
 exec bash
-echo "eval "$(zoxide init --cmd cd bash)""
+echo "eval "$(zoxide init --cmd cd bash)"" >> .bashrc
 exec bash
