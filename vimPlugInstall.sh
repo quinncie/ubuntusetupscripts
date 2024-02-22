e1c0146f58aedec4c800b39c9099e69882054881
Sage <148996861+lungless-chest@users.noreply.github.com>
2024-02-22 10:21:36 -0700

    Update vimPlugInstall.sh

--- vimPlugInstall.sh
+++ vimPlugInstall.sh
@@ -1,7 +1,8 @@
  sudo apt-get update
  sudo apt-get install -y neovim
  curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
- curl -fLo ~/.config/nvim/init.vim --create-dirs     https://raw.githubusercontent.com/quinncie/ubuntusetupscripts/main/assets/init.vim
+ curl -fLo ~/.config/nvim/init.vim --create-dirs     https://raw.githubusercontent.com/lungles-chest/neovim-plug/main/init.vim
  curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
+ sleep 2
  sudo apt-get install -y nodejs
  nvim
