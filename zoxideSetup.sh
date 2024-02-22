be9a56aa4a570b7b3bb3309649d66dd98aa9ca43
Sage <148996861+lungless-chest@users.noreply.github.com>
2024-02-22 10:28:00 -0700

    Update zoxideSetup.sh
    makes work i think

--- zoxideSetup.sh
+++ zoxideSetup.sh
@@ -2,8 +2,7 @@
  sudo apt upgrade -y
  curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
  sudo apt-get install -y fzf
- echo 'export PATH=/home/$USER/.local/bin:"$PATH"' >> ~/.bashrc
- source ~/.bashrc
+ echo "export PATH=/home/$USER/.local/bin:\$PATH" >> ~/.bashrc
  echo 'eval "$(zoxide init --cmd cd bash)"' >> ~/.bashrc
  source ~/.bashrc
  exec bash
