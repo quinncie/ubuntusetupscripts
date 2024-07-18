ea048b1a979d4af212e1644977d292cf7e191bd5
Quinn C. <160205419+quinncie@users.noreply.github.com>
2024-07-18 00:22:39 -0700

    Update lazyVimSetup.sh

--- lazyVimSetup.sh
+++ lazyVimSetup.sh
@@ -1,5 +1,14 @@
-sudo apt-get install ninja-build gettext cmake g++ unzip curl git-all
+sudo apt-get
+sudo apt-get install ninja-build gettext cmake g++ unzip curl git-all libstdc++6
 git clone https://github.com/neovim/neovim
 cd neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo 
 sudo make install 
+cd ~
+git clone https://github.com/ryanoasis/nerd-fonts.git
+cd nerd-fonts
+./install.sh
+cd ~
+sudo rm -r nerd-fonts
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+nvim
